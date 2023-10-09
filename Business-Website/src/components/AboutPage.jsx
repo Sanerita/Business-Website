fa4af8a130475de7db5a4879a0c8f37b6bfdc772
Sanelisiwe Sileku <sanelisiwe.sileku@gmail.com>
2023-10-09 11:21:44 +0200

    added a newsletter section on the about page

--- Business-Website/src/components/AboutPage.jsx
+++ Business-Website/src/components/AboutPage.jsx
@@ -6,6 +6,17 @@ const AboutPage = () => {
     <div id='about'>
     <div className='about'>
     <h2>About Us</h2>
+
+    {/* <section id="newsletter"> */}
+      <div class="news-container" id="newsletter" >
+        <h1>Subscribe To Our Newsletter</h1>
+        <form>
+          <input type="email" placeholder="Enter Email..." required/>
+          <button type="submit" className="button_1">Subscribe</button>
+        </form>
+      </div>
+    {/* </section> */}
+
     <div className="about-image">
         <img src={Smilingchef} alt="" />
       </div>
